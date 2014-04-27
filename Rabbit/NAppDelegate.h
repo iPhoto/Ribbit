efc5279514f84a775e70b472a9cbe7855f1daeb8
Hazel512 <nan.s512@gmail.com>
2014-04-27 11:25:10 +0800

    Initial Commit

---
+++ Rabbit/NAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NAppDelegate.h
+//  Rabbit
+//
+//  Created by Hazel Jiang on 4/27/14.
+//  Copyright (c) 2014 Hazel Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rabbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Rabbit
+//
+//  Created by Hazel Jiang on 4/27/14.
+//  Copyright (c) 2014 Hazel Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NAppDelegate class]));
+    }
+}
